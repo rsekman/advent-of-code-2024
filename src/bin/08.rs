2c39a359c6a334af65afcc733a591032eae55934
Robin Ekman <robin.seth.ekman@gmail.com>
2024-12-11 19:11:11 +0100

    simplify code for day 08

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,15 +1,18 @@
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::io::prelude::*;
 
 use itertools::FoldWhile::{Continue, Done};
 use itertools::Itertools;
-use std::collections::{BTreeMap, BTreeSet};
+
+use coordinates::two_dimensional::Vector2;
+type Point = Vector2<isize>;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let stdin = std::io::stdin();
     let stdin = stdin.lock();
 
-    let mut antennae = BTreeMap::<char, BTreeSet<(isize, isize)>>::new();
+    let mut antennae = HashMap::<char, HashSet<Point>>::new();
     let mut size = (0, 0);
     for (row, line) in stdin.lines().enumerate() {
         for (col, c) in line?.chars().enumerate() {
@@ -18,43 +21,42 @@ fn main() -> Result<(), Box<dyn Error>> {
                 continue;
             }
             if let Some(r) = antennae.get_mut(&c) {
-                r.insert((row as isize, col as isize));
+                r.insert((row as isize, col as isize).into());
             } else {
-                antennae.insert(c, BTreeSet::from([(row as isize, col as isize)]));
+                antennae.insert(c, HashSet::from([(row as isize, col as isize).into()]));
             }
         }
     }
+    let size = (size.0 as isize, size.1 as isize);
 
-    let validate_antinode = |r, c| {
-        if 0 <= r && r < size.0 as isize && 0 <= c && c < size.1 as isize {
-            Some((r, c))
+    let validate_antinode = |n: Point| {
+        if 0 <= n.x && n.x < size.0 as isize && 0 <= n.y && n.y < size.1 {
+            Some(n)
         } else {
             None
         }
     };
 
-    let mut antinodes = BTreeSet::<(isize, isize)>::new();
-    let mut antinodes_harmonics = BTreeSet::<(isize, isize)>::new();
+    let mut antinodes = HashSet::<Point>::new();
+    let mut antinodes_harmonics = HashSet::<Point>::new();
 
     let mut add_antinode = |n| antinodes.insert(n);
-    let mut add_resonant_antinodes = |start, delta: (isize, isize)| {
+    let mut add_resonant_antinodes = |start: Point, delta: Point| {
         std::iter::repeat(delta).fold_while(start, |n, d| {
             antinodes_harmonics.insert(n);
-            validate_antinode(n.0 + d.0, n.1 + d.1)
-                .map(Continue)
-                .unwrap_or(Done(n))
+            validate_antinode(n + d).map(Continue).unwrap_or(Done(n))
         })
     };
     for (_, positions) in antennae.iter() {
         for pair in positions.iter().combinations(2) {
             let (a, b) = (*pair[0], *pair[1]);
-            let (dx, dy) = (a.0 - b.0, a.1 - b.1);
+            let d = a - b;
 
-            validate_antinode(a.0 + dx, a.1 + dy).map(&mut add_antinode);
-            validate_antinode(b.0 - dx, b.1 - dy).map(&mut add_antinode);
+            validate_antinode(a + d).map(&mut add_antinode);
+            validate_antinode(b - d).map(&mut add_antinode);
 
-            let _ = &mut add_resonant_antinodes(a, (dx, dy));
-            let _ = &mut add_resonant_antinodes(b, (-dx, -dy));
+            let _ = &mut add_resonant_antinodes(a, d);
+            let _ = &mut add_resonant_antinodes(b, -d);
         }
     }
 
